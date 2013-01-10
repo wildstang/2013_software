1ba3d015fcc19927d16e67c7ac6ea2fcb4bdd171
unknown <ChadS@chads.itsgames.com>
2013-01-10 12:24:01 -0600

    Created projects for Robot framework and simulation

---
+++ WsSimulation/src/com/wildstangs/simulation/WsSimulation.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wildstangs.simulation;
+
+/**
+ *
+ * @author ChadS
+ */
+public class WsSimulation {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        
+        
+        while (true) {
+
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                
+            }
+            
+        }
+        
+    }
+}
+
