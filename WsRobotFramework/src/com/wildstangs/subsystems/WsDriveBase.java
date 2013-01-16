fc607ddf13136e963376afc43ad440de37c36218
Nathan <Nathan@NathansLaptop>
2013-01-15 18:12:21 -0600

    Added the DriveBase subsystem

---
+++ WsRobotFramework/src/com/wildstangs/subsystems/WsDriveBase.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wildstangs.subsystems;
+
+import com.wildstangs.subjects.base.IObserver;
+import com.wildstangs.subjects.base.Subject;
+import com.wildstangs.subsystems.base.WsSubsystem;
+
+/**
+ *
+ * @author Nathan
+ */
+public class WsDriveBase extends WsSubsystem implements IObserver {
+
+    public WsDriveBase(String name) {
+        super(name);
+    }
+
+    public void acceptNotification(Subject subjectThatCaused) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public void update() {
+        
+    }
+}
