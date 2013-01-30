0794420a05634ea223869a5fa9ed7908b31d1968
Rick <>
2013-01-29 19:48:23 -0500

    Added functionality to WsHopper subsystem.

--- cRioTarget/src/com/wildstangs/subsystems/WsHopper.java
+++ cRioTarget/src/com/wildstangs/subsystems/WsHopper.java
@@ -1,24 +1,78 @@
 package com.wildstangs.subsystems;
 
 import com.wildstangs.subsystems.base.WsSubsystem;
+import com.wildstangs.inputfacade.base.WsInputFacade;
+import com.wildstangs.inputfacade.inputs.joystick.manipulator.WsManipulatorJoystickButtonEnum;
+import com.wildstangs.outputfacade.base.IOutputEnum;
+import com.wildstangs.outputfacade.base.WsOutputFacade;
+import com.wildstangs.subjects.base.Subject;
+import com.wildstangs.subjects.base.BooleanSubject;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import com.wildstangs.subjects.base.IObserver;
+
+
 
 /**
  *
  * @author Liam Fruzyna
  */
-public class WsHopper extends WsSubsystem {
 
-    public WsHopper (String name) {
+public class WsHopper extends WsSubsystem implements IObserver
+{
+    private static final boolean kicker_default_value = false;
+    private static final DoubleSolenoid.Value lift_default_value = DoubleSolenoid.Value.kReverse;
+    private boolean kicker_value;
+    private DoubleSolenoid.Value lift_value;
+    
+    public WsHopper (String name)
+    {
         super(name);
-   }
+        init();
+        
+        Subject subject = WsInputFacade.getInstance().getOiInput(WsInputFacade.MANIPULATOR_JOYSTICK).getSubject(WsManipulatorJoystickButtonEnum.BUTTON2);
+        subject.attach(this);
+        
+        subject = WsInputFacade.getInstance().getOiInput(WsInputFacade.MANIPULATOR_JOYSTICK).getSubject(WsManipulatorJoystickButtonEnum.BUTTON3);
+        subject.attach(this);
+    }
     
     public void init()
     {
+        kicker_value = kicker_default_value;
+        lift_value = lift_default_value;
     }
 
-    public void update() {
+    public void update() 
+    {
+        BooleanSubject button2 = (BooleanSubject)(WsInputFacade.getInstance().getOiInput(WsInputFacade.MANIPULATOR_JOYSTICK).getSubject(WsManipulatorJoystickButtonEnum.BUTTON2));
+        BooleanSubject button3 = (BooleanSubject)(WsInputFacade.getInstance().getOiInput(WsInputFacade.MANIPULATOR_JOYSTICK).getSubject(WsManipulatorJoystickButtonEnum.BUTTON3));
+
+        kicker_value = button2.getValue();
+        if(lift_value.value == DoubleSolenoid.Value.kReverse_val)
+        {
+            kicker_value = false;
+        }
+        
+        if(button3.getValue() == true && (button3.getPreviousValue() == false))
+        {
+            if(lift_value.value == DoubleSolenoid.Value.kReverse_val)
+            {
+                lift_value = DoubleSolenoid.Value.kForward;
+            }
+            else
+            {
+                lift_value = DoubleSolenoid.Value.kReverse;
+            }
+        }
+        WsOutputFacade.getInstance().getOutput(WsOutputFacade.KICKER).set((IOutputEnum)null, new Boolean(kicker_value));
+        WsOutputFacade.getInstance().getOutput(WsOutputFacade.LIFT).set((IOutputEnum)null, lift_value);
     }
 
-    public void notifyConfigChange() {
+    public void notifyConfigChange() 
+    {
+    }
+
+    public void acceptNotification(Subject subjectThatCaused) 
+    {
     }
 }
