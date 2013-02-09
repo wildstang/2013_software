0b0391e050f43917fddff539aa5d57908d9d27c5
Liam Fruzyna <liamfruzyna@gmail.com>
2013-02-09 10:48:37 -0600

    Did the Floor Pickup autonomous step. Not sure if it works

---
+++ cRioTarget/src/com/wildstangs/autonomous/steps/floorpickup/WsAutonomousStepFloorPickupSetSolenoidState.java
@@ -0,0 +1,35 @@
+package com.wildstangs.autonomous.steps.floorpickup;
+
+import com.wildstangs.autonomous.WsAutonomousStep;
+import com.wildstangs.inputfacade.base.WsInputFacade;
+import com.wildstangs.inputfacade.inputs.joystick.driver.WsDriverJoystickButtonEnum;
+import com.wildstangs.subjects.base.BooleanSubject;
+import com.wildstangs.subjects.base.Subject;
+import com.wildstangs.subsystems.WsFloorPickup;
+import com.wildstangs.subsystems.base.WsSubsystemContainer;
+
+/**
+ *
+ * @author Liam Fruzyna
+ */
+public class WsAutonomousStepFloorPickupSetSolenoidState extends WsAutonomousStep 
+{
+    public void initialize()
+    {
+        WsFloorPickup subsystem = (WsFloorPickup)(WsSubsystemContainer.getInstance().getSubsystem(WsSubsystemContainer.WS_FLOOR_PICKUP));
+        Subject subject = WsInputFacade.getInstance().getOiInput(WsInputFacade.DRIVER_JOYSTICK).getSubject(WsDriverJoystickButtonEnum.BUTTON4);
+        BooleanSubject backButton = (BooleanSubject)subject;
+        
+        if(subsystem.getSolenoidState())
+            backButton.setValue(true);
+    }
+    public void update()
+    {
+        
+    }
+    public String toString()
+    {
+        return "doing something with the solenoid";
+    }
+    
+}
