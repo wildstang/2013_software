12a4759088948d2f6873e643433a45783e286769
Joey <Joey@10.186.1.100>
2013-02-18 18:41:46 -0600

    Added the "wait for funnelator limit switch true to false" and the "wait for accumulator left and right limit switches" autonomous steps

---
+++ cRioTarget/src/com/wildstangs/autonomous/steps/intake/WsAutonomousStepWaitForFunnelatorLimitSwitchTrueToFalse.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wildstangs.autonomous.steps.intake;
+
+import com.wildstangs.autonomous.WsAutonomousStep;
+import com.wildstangs.inputfacade.base.WsInputFacade;
+import com.wildstangs.subjects.base.BooleanSubject;
+import com.wildstangs.subjects.base.IObserver;
+import com.wildstangs.subjects.base.ISubjectEnum;
+import com.wildstangs.subjects.base.Subject;
+
+/**
+ *
+ * @author Joey
+ */
+public class WsAutonomousStepWaitForFunnelatorLimitSwitchTrueToFalse extends WsAutonomousStep implements IObserver
+{
+    
+    public WsAutonomousStepWaitForFunnelatorLimitSwitchTrueToFalse()
+    {
+        Subject subject = WsInputFacade.getInstance().getSensorInput(WsInputFacade.FUNNELATOR_LIMIT_SWITCH).getSubject((ISubjectEnum) null);
+        subject.attach(this);
+    }
+
+    public void initialize()
+    {
+        
+    }
+
+    public void update()
+    {
+        
+    }
+
+    public String toString()
+    {
+        return "Waiting for funnelator limit switch to go from true to false";
+    }
+
+    public void acceptNotification(Subject subjectThatCaused) 
+    {
+        if(((BooleanSubject) subjectThatCaused).getValue() == false)
+        {
+            finished = true;
+        }
+    }
+}
